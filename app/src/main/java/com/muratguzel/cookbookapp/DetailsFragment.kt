1e89c9b3a84d3834b8aee8df9a1825f533058e34
murat-guzel33 <murat.guzel4004@gmail.com>
2024-07-24 23:19:09 +0300

    Fragment navigation and argument added

--- app/src/main/java/com/muratguzel/cookbookapp/DetailsFragment.kt
+++ app/src/main/java/com/muratguzel/cookbookapp/DetailsFragment.kt
@@ -5,9 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.muratguzel.cookbookapp.databinding.FragmentDetailBinding
+import com.muratguzel.cookbookapp.databinding.FragmentListBinding
 
 class DetailsFragment : Fragment() {
-
+    private var _binding: FragmentDetailBinding? = null
+    private val binding get() = _binding!!
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         arguments?.let {
@@ -17,15 +20,55 @@ class DetailsFragment : Fragment() {
 
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail, container, false)
+        _binding = FragmentDetailBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.imageView.setOnClickListener { selectImage(it) }
+        binding.saveButton.setOnClickListener { save(it) }
+        binding.deleteButton.setOnClickListener { delete(it) }
+        arguments?.let {
+            val info = DetailsFragmentArgs.fromBundle(it).info
+            if (info == "new") {
+                // new recipe will be added
+                binding.deleteButton.isEnabled = false
+                binding.saveButton.isEnabled = true
+                binding.cookNameText.setText("")
+                binding.materialText.setText("")
+                binding.imageView.setImageResource(R.drawable.ic_launcher_background)
+            } else {
+                //The old added tariff will be checked.
+                binding.saveButton.isEnabled = false
+                binding.deleteButton.isEnabled = true
+                binding.imageView.isEnabled = false
+                binding.cookNameText.isEnabled = false
+                binding.materialText.isEnabled = false
+            }
+        }
     }
 
+    fun save(view: View) {
+
+    }
+
+    fun delete(view: View) {
+
+    }
+
+    fun selectImage(view: View) {
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
+
--- app/src/main/java/com/muratguzel/cookbookapp/ListFragment.kt
+++ app/src/main/java/com/muratguzel/cookbookapp/ListFragment.kt
@@ -5,8 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import com.muratguzel.cookbookapp.databinding.FragmentListBinding
 
 class ListFragment : Fragment() {
+    private var _binding: FragmentListBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,15 +20,25 @@ class ListFragment : Fragment() {
     }
 
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?,
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
+        _binding = FragmentListBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.floatingActionButton.setOnClickListener {
+            val action = ListFragmentDirections.actionListFragmentToDetailsFragment(info = "new",id=0)
+            Navigation.findNavController(view).navigate(action)
+        }
     }
 
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
